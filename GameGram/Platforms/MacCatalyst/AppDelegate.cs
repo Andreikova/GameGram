00f38ccbc157ab11c8dc12a8d21fac490e8b30ba
Andreikova <andrei.khoudiakov@student.nhlstenden.com>
2025-04-01 17:13:44 +0200

    Base program

---
+++ GameGram/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GameGram
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
